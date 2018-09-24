ce1f9f5bc6763e8420c8de56edda3d87c9211739
Ulich, Timo <timo@ulich.net>
2018-09-24 22:23:16 +0200

    fix heatmap

--- src/pages/dashboard/components/Map.js
+++ src/pages/dashboard/components/Map.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import HeatmapLayer from "react-google-maps/lib/components/visualization/HeatmapLayer";
 import MapMarker from "./MapMarker";
 
 const url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCU6Dz9ZWKxO8fkco490PHhipeqH2u13iw&v=3.exp&libraries=visualization,geometry,drawing,places"
-const testData = [
-  {lat: -34, lng: 151}
-]
-const RawMap = compose(withScriptjs(withGoogleMap(({ sensors, badSpotSensor = {}, selectedSensor = {}, onSensorClick }) => (
-  
+const RawMap = withScriptjs(withGoogleMap(({ sensors, badSpotSensor = {}, selectedSensor = {}, onSensorClick }) => (
+    
     <GoogleMap defaultZoom={14}
                defaultCenter={{ lat: 53.8554374, lng: 10.6783012 }}>
-                googleMapURL={url}
                {sensors.map(sensor =>( 
                <MapMarker sensor={sensor}
                  badSpot={sensor.sensorId === badSpotSensor.sensorId}
@@ -20,9 +15,12 @@ const RawMap = compose(withScriptjs(withGoogleMap(({ sensors, badSpotSensor = {}
                  onClick={onSensorClick}
                  key={sensor.sensorId} />
                ))}
-               <HeatmapLayer data={testData}/>
+               <HeatmapLayer options={{radius: 100}} data={[
+                   { location: new window.google.maps.LatLng(53.8669208, 10.6833571), weight: 1 },
+                   { location: new window.google.maps.LatLng(53.849174, 10.6722478), weight: 3 },
+               ]} />
     </GoogleMap>
-))))
+)))
 
 export default function Map({ style, ...rest }) {
     return (
