f27c00c33b50e39c0815ecf65b599cf506d91237
Thilko Richter <me@thilko.com>
2018-09-30 18:03:32 +0200

    Small bar at the top for important information

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import SensorMetrics from './components/SensorMetrics';
 import Map from './components/Map';
-import {loadSensors, loadBadSpot, loadBadSpotHistory, loadUndersuppliedSpots} from '../../services/SensorClient';
+import {loadSensors, loadBadSpot, loadBadSpotHistory, loadUndersuppliedSpots, fetchPressureThreshold } from '../../services/SensorClient';
 import LoadingScreen from './components/LoadingScreen';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 export default class Dashboard extends React.Component {
 
@@ -22,6 +26,7 @@ export default class Dashboard extends React.Component {
     const badSpot = await loadBadSpot()
     const badSpotHistory = await loadBadSpotHistory()
     const undersuppliedSpots = await loadUndersuppliedSpots()
+    const pressureThreshold = await fetchPressureThreshold()
 
     if(badSpot.underSupplied){
       (new Audio('/beep.mp3')).play().catch(error => {
@@ -35,19 +40,19 @@ export default class Dashboard extends React.Component {
       sensors,
       badSpot,
       badSpotHistory,
-      undersuppliedSpots
+      undersuppliedSpots,
+      pressureThreshold
     })
   }
 
   handleSensorClick = (sensorId) => {
-    console.log(sensorId)
     this.setState({
       selectedSensorId: sensorId
     })
   }
 
   render() {
-    const {sensors,badSpot, selectedSensorId, badSpotHistory, undersuppliedSpots} = this.state
+    const {sensors,badSpot, selectedSensorId, badSpotHistory, undersuppliedSpots, pressureThreshold} = this.state
     if (!sensors) {
       return <LoadingScreen />
       }
@@ -55,14 +60,34 @@ export default class Dashboard extends React.Component {
     const badSpotSensor = sensors.filter(s => s.sensorId === badSpot.sensor.sensorId)[0]
     const selectedSensor = sensors.filter(s => s.sensorId === selectedSensorId)[0]
 
-    return <div style={{ display: 'flex', height: '100vh' }}>
+    return <div style={{ display: 'flex', height: '100vh', flexDirection: 'row wrap', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', width: '100%', height: '10%', justifyContent: 'center'}}>
+        <Card style={{ flex: '0 15%', height: '100%'}}>
+          <CardContent>
+            <Typography>Pressure Threshold</Typography>
+            <Typography variant="headline" component="h2">{ pressureThreshold } mBar</Typography>
+          </CardContent>
+        </Card>
+        <Card style={{ flex: '0 15%', height: '100%'}}>
+          <CardContent>
+            <Typography>Bad Spot Pressure</Typography>
+            <Typography variant="headline" component="h2">{ badSpot.measurement.value } mBar</Typography>
+          </CardContent>
+        </Card>
+        <Card style={{ flex: '0 15%', height: '100%'}}>
+          <CardContent>
+            <Typography>Undersupplied Spots</Typography>
+            <Typography variant="headline" component="h2">{ undersuppliedSpots.sensors.length }</Typography>
+          </CardContent>
+        </Card>
+      </div>
       <Map sensors={sensors}
         badSpotSensor={badSpotSensor}
         badSpotHistory={badSpotHistory}
         undersuppliedSpots={undersuppliedSpots}
         selectedSensor={selectedSensor}
         onSensorClick={this.handleSensorClick}
-        style={{flex: 1}} />
+        style={{height: '90%', flex: '1 70%'}} />
 
       <SensorMetrics sensor={selectedSensor} style={{ width: '30%' }}/>
     </div>
